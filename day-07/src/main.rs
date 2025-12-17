2320f40393b7e9674e845eabdc99930c0994a4b0
TimPaasche <tim.paasche@outlook.de>
2025-12-17 22:34:29 +0100

    fmt

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -8,17 +8,17 @@ fn main() {
         return;
     }
 
-    for row in 0..grid.len() -1 {
+    for row in 0..grid.len() - 1 {
         for col in 0..grid[row].len() {
             if grid[row][col] == 'S' {
-                grid[row+1][col] = '|';
+                grid[row + 1][col] = '|';
             }
-            if grid[row][col] == '|' && row < grid.len() && grid[row+1][col] == '.'{
-                grid[row+1][col] = '|';
+            if grid[row][col] == '|' && row < grid.len() && grid[row + 1][col] == '.' {
+                grid[row + 1][col] = '|';
             }
-            if grid[row][col] == '|' && row < grid.len() && grid[row+1][col] == '^'{
-                grid[row+1][col-1] = '|';
-                grid[row+1][col+1] = '|';
+            if grid[row][col] == '|' && row < grid.len() && grid[row + 1][col] == '^' {
+                grid[row + 1][col - 1] = '|';
+                grid[row + 1][col + 1] = '|';
             }
         }
     }
@@ -27,13 +27,13 @@ fn main() {
 
     for row in 1..grid.len() {
         for col in 0..grid[row].len() {
-            if grid[row][col] == '^' && grid[row-1][col] == '|' {
-               count += 1;
+            if grid[row][col] == '^' && grid[row - 1][col] == '|' {
+                count += 1;
             }
         }
     }
     println!("count: {}", count);
-    
+
     let mut result: String = String::new();
     for row in 0..grid.len() {
         for col in 0..grid[row].len() {
@@ -43,6 +43,4 @@ fn main() {
     }
 
     write_result_file(&result);
-
-
 }

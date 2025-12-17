86a88992650226ece584d31fcea74f6cfc9ab8e4
TimPaasche <tim.paasche@outlook.de>
2025-12-17 22:33:41 +0100

    day-07 pt 1

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,7 +1,48 @@
 use tools::read_input_file;
+use tools::write_result_file;
 
 fn main() {
     let input = read_input_file();
-    let lines: Vec<&str> = input.lines().collect();
-    println!("{:#?}", lines);
+    let mut grid: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+    if grid.is_empty() {
+        return;
+    }
+
+    for row in 0..grid.len() -1 {
+        for col in 0..grid[row].len() {
+            if grid[row][col] == 'S' {
+                grid[row+1][col] = '|';
+            }
+            if grid[row][col] == '|' && row < grid.len() && grid[row+1][col] == '.'{
+                grid[row+1][col] = '|';
+            }
+            if grid[row][col] == '|' && row < grid.len() && grid[row+1][col] == '^'{
+                grid[row+1][col-1] = '|';
+                grid[row+1][col+1] = '|';
+            }
+        }
+    }
+
+    let mut count: i64 = 0;
+
+    for row in 1..grid.len() {
+        for col in 0..grid[row].len() {
+            if grid[row][col] == '^' && grid[row-1][col] == '|' {
+               count += 1;
+            }
+        }
+    }
+    println!("count: {}", count);
+    
+    let mut result: String = String::new();
+    for row in 0..grid.len() {
+        for col in 0..grid[row].len() {
+            result.push(grid[row][col]);
+        }
+        result.push('\n');
+    }
+
+    write_result_file(&result);
+
+
 }
--- tools/src/lib.rs
+++ tools/src/lib.rs
@@ -16,3 +16,17 @@ pub fn read_input_file() -> String {
         fs::read_to_string(&path).expect(&format!("Failed to read file: {}", path));
     contents_with_bom.trim_start_matches('\u{FEFF}').to_string()
 }
+
+pub fn write_result_file(result: &str){
+    let path = format!(
+        "{}{}src{}result.txt",
+        env::current_dir()
+            .expect("Failed to get current directory")
+            .to_str()
+            .expect("Failed to convert path to string"),
+        path::MAIN_SEPARATOR,
+        path::MAIN_SEPARATOR
+    );
+    
+    fs::write(&path, result).expect(&format!("Failed to write file: {}", path));
+}

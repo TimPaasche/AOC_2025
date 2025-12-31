8df2c06aa5b5f0c9e8da0227ff8dd4f6a90874fe
TimPaasche <tim.paasche@outlook.de>
2025-12-31 12:32:05 +0100

    started to code the pt2 of day-10

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -6,22 +6,29 @@ use tools::read_input_file;
 struct Instruction {
     goal: u16,
     buttons: Vec<u16>,
-    rest: String,
+    joltage_values: Vec<u16>,
 }
 
 fn main() {
     let input: String = read_input_file();
     let lines: Vec<&str> = input.lines().collect();
-    let mut count: u32 = 0;
+    let mut count_startup: u32 = 0;
+    let mut count_joltage_value: u32 = 0;
     for line in lines {
-        count += minimal_moves_to_resolve(line);
+        count_startup += minimal_moves_to_resolve_startup(line);
+        count_joltage_value += minimal_moves_to_resolve_joltage_value(line);
     }
-    println!("count: {:?}", count);
+    println!("count: {:?}", count_startup);
 }
 
-fn minimal_moves_to_resolve(input_line: &str) -> u32 {
+fn minimal_moves_to_resolve_startup(input_line: &str) -> u32 {
     let instructions = parse_line(input_line);
-    breadth_first_search(instructions)
+    bfs_startup(instructions)
+}
+
+fn minimal_moves_to_resolve_joltage_value(input_line: &str) -> u32 {
+    let instruction = parse_line(input_line);
+    bfs_joltage_value(instruction)
 }
 
 fn parse_line(line: &str) -> Instruction {
@@ -46,12 +53,23 @@ fn parse_line(line: &str) -> Instruction {
     }
 
     //parsing the rest
-    let rest = parts.last().unwrap();
+    let part_last = parts.last().unwrap();
+    let joltage_values: Vec<u16> = parse_joltage_values(part_last);
     Instruction {
         goal,
         buttons,
-        rest: rest.to_string(),
+        joltage_values,
     }
+}
+fn parse_joltage_values(joltage_str: &str) -> Vec<u16> {
+    let mut rtn_joltage_values = Vec::new();
+    let joltige_str_trimmed: &str = joltage_str.trim_matches(|c| c == '{' || c == '}');
+    joltige_str_trimmed.split(',').for_each(|joltage_value| {
+        if let Ok(val) = joltage_value.parse::<u16>() {
+            rtn_joltage_values.push(val);
+        }
+    });
+    rtn_joltage_values
 }
 
 fn parse_button_str(button_str: &str) -> u16 {
@@ -66,7 +84,7 @@ fn parse_button_str(button_str: &str) -> u16 {
     btn_mask
 }
 
-fn breadth_first_search(content: Instruction) -> u32 {
+fn bfs_startup(content: Instruction) -> u32 {
     // Queue stores: (current_light_state, number_of_presses_so_far)
     let mut queue: VecDeque<(u16, u32)> = VecDeque::new();
     let mut visited: HashSet<u16> = HashSet::new();
@@ -95,4 +113,16 @@ fn breadth_first_search(content: Instruction) -> u32 {
     }
 
     panic!("No solution found for this machine!");
+}
+
+fn bfs_joltage_value(content: Instruction) -> u32 {
+    let number_of_joltage_values: usize = content.joltage_values.len();
+    // Queue stores: (current_joltage_state, number_of_presses_so_far)
+    let mut queue: VecDeque<(Vec<u16>, u32)> = VecDeque::new();
+    let mut visited: HashSet<u16> = HashSet::new();
+
+    // We start with all lights OFF (state 0) and 0 presses.
+    queue.push_back((vec![0; number_of_joltage_values], 0));
+    visited.insert(0);
+    todo!()
 }
